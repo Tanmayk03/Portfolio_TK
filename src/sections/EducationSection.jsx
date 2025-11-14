2463948f462f0a78ab0ceb4fce87993c44ec3649
Tanmay Kapoor <142162139+Tanmayk03@users.noreply.github.com>
2025-11-15 04:12:59 +0530

    Update in project section

--- src/sections/EducationSection.jsx
+++ src/sections/EducationSection.jsx
@@ -114,7 +114,7 @@ const EducationSection = () => {
       {/* Cool animated background */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
         <div className="absolute top-32 right-20 w-80 h-80 bg-indigo-500/15 rounded-full blur-3xl animate-pulse" />
-        <div className="absolute bottom-32 left-20 w-96 h-96 bg-white-500/15 rounded-full blur-3xl animate-pulse delay-1000" />
+        <div className="absolute bottom-32 left-20 w-96 h-96 bg--500/15 rounded-full blur-3xl animate-pulse delay-1000" />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[400px] bg-purple-500/10 rounded-full blur-3xl" />
       </div>
 
